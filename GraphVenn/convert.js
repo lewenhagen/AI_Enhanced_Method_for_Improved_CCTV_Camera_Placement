55161ae4243931138ac7a6356e55c7e924b8052d
lewenhagen <lewenhagen@live.com>
2026-03-10 11:18:18 +0100

    sadf

--- GraphVenn/convert.js
+++ GraphVenn/convert.js
@@ -1,15 +1,15 @@
 import fs from "fs";
-
-const lines = fs.readFileSync("GraphVenn_result_top30_optimal_Ystad_d=100_p=4.csv", "utf8").trim().split("\n");
+let name = "Lund"
+const lines = fs.readFileSync(`GraphVenn_result_top30_optimal_${name}_d=100_p=4.csv`, "utf8").trim().split("\n");
 
 const result = lines
   .slice(1)
   .map(line => {
-    const [, , lat, lon] = line.split(",");
-    return `${lat.trim()}, ${lon.trim()}`;
+    const [rank, , lat, lon] = line.split(",");
+    return `${rank}, ${lat.trim()}, ${lon.trim()}`;
   })
   .filter(Boolean);
 
-fs.writeFileSync("ystad.json", JSON.stringify(result, null, 2));
+fs.writeFileSync(`${name.toLowerCase()}.json`, JSON.stringify(result, null, 2));
 
-console.log("coords.json created");
+console.log(`${name.toLowerCase()}.json created`);
